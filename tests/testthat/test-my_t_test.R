98a92e34b24f03b82989906e2238e35e410d5b52
oliverlinehan41 <olinehan@gmail.com>
2021-06-08 14:55:11 -0700

    my_t_test test

--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -2,8 +2,8 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
-test_that("Degrees of Freedom is closure", {
-  expect_type(object = df, type = "closure")
+test_that("Standard Error is Numeric", {
+  expect_type(my_t_test(my_gapminder$lifeExp, "two.sided", 60), type = "list")
 })
 
 

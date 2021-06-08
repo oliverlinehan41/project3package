a4f653d1e35fd3248418ecc6cacf91211655f531
oliverlinehan41 <olinehan@gmail.com>
2021-06-08 14:26:15 -0700

    test edit

--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -6,6 +6,4 @@ test_that("Degrees of Freedom is closure", {
   expect_type(object = df, type = "closure")
 })
 
-test_that("Alternative is character", {
-  expect_type(object = my_t_test(my_gapminder, "two.sided", 60), type = "list")
-})
+

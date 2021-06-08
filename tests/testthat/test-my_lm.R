d050d2f772fbf8ca395453d458c1a15d8f08223b
oliverlinehan41 <olinehan@gmail.com>
2021-06-08 15:07:13 -0700

    my_lm test

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -2,6 +2,6 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
-test_that("Function returns table", {
-  expect_type(object = my_lm(lifeExp ~ gdpPercap + continent, my_gapminder))
+test_that("Function returns double", {
+  expect_type(object = my_lm(lifeExp ~ gdpPercap + continent, my_gapminder), type = "double")
 })

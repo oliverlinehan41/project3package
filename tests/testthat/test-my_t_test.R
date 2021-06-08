3c3a72c5c86899cbf536711d8f7947d56057714a
oliverlinehan41 <olinehan@gmail.com>
2021-06-08 15:03:18 -0700

    t test test edit

--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -2,7 +2,7 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
-test_that("Standard Error is Numeric", {
+test_that("List is returned", {
   expect_type(my_t_test(my_gapminder$lifeExp, "two.sided", 60), type = "list")
 })
 

7e52e23b33d69629bc168763e9009e7cbc19b24b
oliverlinehan41 <olinehan@gmail.com>
2021-06-08 21:09:14 -0700

    knn and lm tests

--- tests/testthat/test-my_knn_cv.R
+++ tests/testthat/test-my_knn_cv.R
@@ -1,3 +1,8 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+test_that("Function returns list", {
+  expect_type(object = my_knn_cv(my_penguins, my_penguins$species, 1, 5),
+              type = "list")
+})
--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -5,3 +5,4 @@ test_that("multiplication works", {
 test_that("Function returns double", {
   expect_type(object = my_lm(lifeExp ~ gdpPercap + continent, my_gapminder), type = "double")
 })
+
